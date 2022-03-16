79c4e05a78217f63179f4d37fcbe90445313529a
elbercaio <elbercaio@gmail.com>
2022-03-16 11:19:31 -0300

    UI

--- javascript.js
+++ javascript.js
@@ -35,29 +35,41 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+function reset() {
+  document.querySelector("#player").textContent = 0;
+  document.querySelector("#computer").textContent = 0;
+  document.querySelector("h3").textContent = "";
+}
+
 function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Please enter your answer: ");
-    round = playRound(playerSelection, computerPlay());
-    console.log(round);
-    if (round.includes("Win")) {
-      playerScore++;
-    } else if (round.includes("Lose")) {
-      computerScore++;
-    }
-    console.log(
-      `Player: ${playerScore} x Computer: ${computerScore}`
-    );
+  round = playRound(this.id, computerPlay());
+  document.querySelector("h3").textContent = round;
+  playerScore = parseInt(document.querySelector("#player").textContent);
+  computerScore = parseInt(document.querySelector("#computer").textContent);
+  console.log(playerScore);
+  if (round.includes("Win")) {
+    playerScore++;
+    document.querySelector("#player").textContent = playerScore;
+  } else if (round.includes("Lose")) {
+    computerScore++;
+    document.querySelector("#computer").textContent = computerScore;
   }
-  if (playerScore > computerScore) {
-    console.log("You won against the machine!");
-  } else if (playerScore < computerScore) {
-    console.log("You lost against the machine. Shame!");
-  } else {
-    console.log("An Overall Draw, unlucky");
+  if (playerScore === 5 || computerScore === 5) {
+    if (playerScore > computerScore) {
+      alert(
+        `${playerScore} X ${computerScore}: You won against the machine. Congrats!`
+      );
+    } else if (playerScore < computerScore) {
+      alert(
+        `${playerScore} X ${computerScore}: You lost against the machine. Shame!`
+      );
+    }
+    reset();
   }
 }
 
-game()
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", game);
+});
